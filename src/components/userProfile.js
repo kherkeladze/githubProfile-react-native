ce92129efc0df892828e2badf6cd07c51731b815
Aka <akaki.kherkeladze@gmail.com>
2016-11-20 06:01:54 +0400

    add user profile fields

--- src/components/userProfile.js
+++ src/components/userProfile.js
@@ -3,6 +3,8 @@
 import React, { Component } from 'react';
 import API from '../api';
 import SearchBar from 'react-native-search-bar';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 import {
   StyleSheet,
@@ -25,7 +27,7 @@ class UserProfile extends Component {
       <View>
         <SearchBar ref='searchBar'
                    placeholder={'Enter Github Username'}
-                   onChangeText={userName => this.setState( { userName: userName } ) }
+                   onChangeText={ userName => this.setState( { userName: userName } ) }
                    onSearchButtonPress={ () => this.getUserData() } />
         { this.renderUserProfile() }
       </View>
@@ -46,8 +48,14 @@ class UserProfile extends Component {
     console.log(this.state);
     if(this.state.userData) {
       return (
-        <View>
+        <View style={ styles.userProfileView }>
           <Image style={ styles.userImage } source={{ uri: this.state.userData.avatar_url }}/>
+          <Text style={ styles.userFullName }>{ this.state.userData.name.toUpperCase() }</Text>
+          <Text> { this.state.userData.bio } </Text>
+
+            <Text>
+              <Icon name="map-marker" size={ 15 } color="#000" /> { this.state.userData.location }
+            </Text>
         </View>
       )
     }
@@ -57,12 +65,23 @@ class UserProfile extends Component {
 const styles = StyleSheet.create({
   userNameInput: {
     height: 20,
-    borderColor: 'gray',
-    borderWidth: 1
+    borderColor: 'gray'
+
+  },
+  userProfileView: {
+    alignItems: 'center',
+    paddingTop: 10
   },
   userImage: {
-      width: 250,
-      height: 250
+      width: 160,
+      height: 160,
+      borderRadius: 80
+  },
+  userFullName: {
+    padding: 10,
+    fontSize: 20,
+    fontFamily: 'Apple SD Gothic Neo',
+    fontWeight: '800'
   }
 });
 
